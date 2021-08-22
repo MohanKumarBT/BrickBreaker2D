52fc45698eac3c179f2790d1a37f63d4694b2c61
Mohan Kumar <btmohankumar3212@gmail.com>
2021-08-23 01:23:01 +0530

    Bricks

---
+++ BrickBreaker2D/Assets/Scripts/BallController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public bool inPlay;
+    public Transform paddle;
+    public float force;
+    public Transform explosion;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    void Update()
+    {
+        if (!inPlay)
+        {
+            transform.position = paddle.position;
+        }
+        if (Input.GetButtonDown("Jump") && !inPlay)
+        {
+            inPlay = true;
+            rb.AddForce(Vector2.up * force);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("bottom"))
+        {
+            Debug.Log("Bottom Hit");
+            rb.velocity = Vector2.zero;
+            inPlay = false;
+        }  
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.transform.CompareTag("brick"))
+        {
+           Transform newExplosion = Instantiate(explosion, other.transform.position, other.transform.rotation);
+            Destroy(newExplosion.gameObject, 2.5f);
+            Destroy(other.gameObject);
+        }
+    }
+}
--- BrickBreaker2D/Assets/Scripts/PaddleController.cs
+++ BrickBreaker2D/Assets/Scripts/PaddleController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PaddleController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed;
+    public float rightscreenedge;
+    public float leftscreenedge;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        transform.Translate(Vector2.right * horizontal * Time.deltaTime * speed);
+        if(transform.position.x < leftscreenedge)
+        {
+            transform.position = new Vector2(leftscreenedge, transform.position.y);
+        }
+        if (transform.position.x > rightscreenedge)
+        {
+            transform.position = new Vector2(rightscreenedge, transform.position.y);
+        }
     }
 }
